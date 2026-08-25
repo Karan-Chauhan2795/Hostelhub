6274502d3dd0d43153d6984e989ca5a9aef031c4
Karan Chauhan <chauhankaran2795@gmail.com>
2026-08-25 18:17:10 +0530

    display live in mobile

--- hostelhub/settings.py
+++ hostelhub/settings.py
@@ -4,7 +4,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-hostelhub-skeleton"
 DEBUG = True
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver"]
+# ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver"]
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
